78ee748e4754ccfac496b0397f998b98cd9e5368
Ahmad Kianfar <ahmadkianfar70@gmail.com>
2025-09-22 12:58:47 +0330

    Alert

--- App.js
+++ App.js
@@ -7,17 +7,38 @@ import {
   TouchableWithoutFeedback,
   TouchableOpacity,
   TouchableHighlight,
-  Button
-
+  Button,
+  Alert,
 } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const handlePress = ()=> console.log("Button preesd");
-  
+  const handlePress = () => console.log("Button preesd");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="click me" onPress={handlePress} color="orange" />
+      <Button
+        title="click me"
+        onPress={() => alert("Button is preesed")}
+        color="orange"
+      />
+      <Button
+        title="click me for Alert"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      />
+      <Button
+        title="click me for prompt"
+        onPress={() =>
+          Alert.prompt("My title", "My message", (text) => console.log(text))
+          // not working on android!
+        }
+        color="purple"
+      />
     </SafeAreaView>
   );
 }
