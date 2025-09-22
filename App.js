6590e115561e35987a3586aecbbbfa1b0f07ffce
Ahmad Kianfar <ahmadkianfar70@gmail.com>
2025-09-22 12:16:11 +0330

    Touchable component

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, SafeAreaViewBase, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight
+
+} from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello React Native!</Text>
-      {/* <Image source={require("./assets/favicon.png")} /> */}
-      <Image
-        blurRadius={5}
-        fadeDuration={5000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableHighlight onPress={()=> console.log("Image Preesed")}>
+        <Image
+          blurRadius={5}
+          fadeDuration={5000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight >
     </SafeAreaView>
   );
 }
