72f714ea1277bcf3e86ab9d8a3920f6cf088ac2b
Ahmad Kianfar <ahmadkianfar70@gmail.com>
2025-09-23 23:30:03 +0330

    Flex Box

--- App.js
+++ App.js
@@ -1,48 +1,31 @@
-import {
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-  Dimensions,
-  useWindowDimensions
-} from "react-native";
-
-
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { View } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Button preesd");
-
-  const {width, height} = useWindowDimensions();
-  const isLandscape = width > height;
-  console.log("width:",width,"height:",{height});
-  console.log(`isLandscape: ${isLandscape}`)
-  
-  
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={{
+       backgroundColor: "white", flex: 1 }}>
+      
       <View
         style={{
-          backgroundColor:"dodgerblue",
-          width: '50%',
-          height: 70,
+          backgroundColor: "dodgerblue",
+          flex: 2,
         }}
-      ></View>
-    </SafeAreaView>
+      />
+
+      <View
+        style={{
+          backgroundColor: "yellow",
+          flex: 1,
+        }}
+      />
+
+      <View
+        style={{
+          backgroundColor: "tomato",
+          flex: 1,
+        }}
+      />
+
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    
-  },
-});
