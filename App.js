4de680797c18e517d51acd5b6566f02d2578bfd7
Ahmad Kianfar <ahmadkianfar70@gmail.com>
2025-09-25 14:38:13 +0330

    Exercise Building the Listing Details Screen

--- App.js
+++ App.js
@@ -5,21 +5,10 @@ import colors from "./app/config/colors";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import AppText from "./app/components/AppText";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale!"
-        subtitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
+    <ListingDetailsScreen />
   );
 }
