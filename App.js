0fe6714417ce04479ea81f2324b3fab9d190119c
Ahmad Kianfar <ahmadkianfar70@gmail.com>
2025-10-15 09:11:31 +0330

    showing-an-offline-notice

--- App.js
+++ App.js
@@ -5,12 +5,15 @@ import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import ListingScreen from "./app/screens/ListingScreen";
-
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
---
+++ app/components/OfflineNotice.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import { useNetInfo } from "@react-native-community/netinfo";
+
+import AppText from "./AppText";
+import colors from "../config/colors";
+
+function OfflineNotice(props) {
+  const netInfo = useNetInfo();
+
+  if (netInfo.type !== "unknown" && netInfo.isInternetReachable === false)
+    return (
+      <View style={styles.container}>
+        <AppText style={styles.text}>No Interntet Connection</AppText>
+      </View>
+    );
+
+  return null;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.primary,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    width: "100%",
+    position: "absolute",
+    top: Constants.statusBarHeight,
+    zIndex: 1,
+  },
+  text: {
+    color: colors.white,
+  },
+});
+
+export default OfflineNotice;
