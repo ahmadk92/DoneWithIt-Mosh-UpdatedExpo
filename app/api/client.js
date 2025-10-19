5a8c4bccee7591fd3baf9211711eae382a11cf76
Ahmad Kianfar <ahmadkianfar70@gmail.com>
2025-10-19 20:41:16 +0330

    calling-protected-apis

--- app/api/client.js
+++ app/api/client.js
@@ -1,9 +1,16 @@
 import { create } from "apisauce";
 import cache from "../utility/cache";
+import authStorage from "../auth/storage";
 
 const apiClient = create({
   baseURL: "http://192.168.1.4:9000/api",
   timeout: 2000,
+});
+
+apiClient.addAsyncRequestTransform(async (request) => {
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
+  request.headers["x-auth-token"] = authToken;
 });
 
 //changing the implementation of get method
@@ -18,7 +25,7 @@ apiClient.get = async (url, params, axiosConfig) => {
   }
 
   const data = await cache.get(url);
-  return data ? { ok: true, data} : response;
+  return data ? { ok: true, data } : response;
 };
 
 export default apiClient;
